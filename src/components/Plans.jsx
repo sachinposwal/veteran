05ad0370fe64221bf3a7a01029ad9d314f7c4759
sachinposwal <sachinposwal222@gmail.com>
2025-03-19 00:06:55 +0530

    plans

--- src/components/Plans.jsx
+++ src/components/Plans.jsx
@@ -24,8 +24,9 @@ function Plans() {
         <div className="pricing-card silver">
             <h4 className="plan-title ">Starter Plan</h4>
             <div className="plan-price">
-                <span className="currency">₹</span><span className="amount">4,600</span>
-                <span className="duration">one-time</span>
+                {/* <span className="currency">₹</span><span className="amount">4,600</span>
+                <span className="duration">one-time</span>*/}
+                
             </div>
             <button className="choose-button" onClick={()=> setPopupActive(true)}>Enroll Now</button>
             <p className="description">
@@ -43,8 +44,9 @@ function Plans() {
         <div className="pricing-card gold">
             <h4 className="plan-title gp">Grow Plan</h4>
             <div className="plan-price">
+                {/* 
                 <span className="currency">₹</span><span className="amount">6,499</span>
-                <span className="duration">monthly</span>
+                <span className="duration">monthly</span> */}
             </div>
             <button className="choose-button" onClick={()=> setPopupActive(true)} >Enroll Now</button>
             <p className="description">
